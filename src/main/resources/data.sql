ee98a1148c30f981439206a27a6ae0d021724beb
swapna <swapna.soni@gmail.com>
2021-02-26 22:05:33 +0530

    implementation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into TOKEN_DETAIL values('a6933d5be409e8db5710714ddc4fecf0', null,0);
+insert into TOKEN_DETAIL values('010fcd619a09ab9ea81c0557c4b290aa', null,0);
+insert into TOKEN_DETAIL values('345dacaa85d3f5f53fe29a91e806aa34', null,0);
+insert into TOKEN_DETAIL values('6acebb19772bf14d107dd7fe059ce962', null,0);
+insert into TOKEN_DETAIL values('cdd75f9136de7e5ca88e345caf824e3a', null,0);
+
+commit;
